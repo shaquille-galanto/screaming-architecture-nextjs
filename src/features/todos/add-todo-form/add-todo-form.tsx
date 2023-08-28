17d2a9c868d3f506fe3eb7f5b1bf1e1597871919
Shaquille Galanto <shaquille.galanto18@gmail.com>
2023-08-28 23:30:50 +0800

    ✨ feat: implement add-todo-form feature

---
+++ src/features/todos/add-todo-form/add-todo-form.tsx
@@ -0,0 +1,39 @@
+'use client'
+
+import { Button, Input } from '@features/ui'
+import { Field, Form, Formik } from 'formik'
+import * as yup from 'yup'
+import { useTodoDispatch } from '../todo-context'
+import styles from './styles.module.scss'
+import type { AddTodoFormProps } from './types'
+
+const formInitialValues = {
+  text: '',
+}
+
+const validationSchema = yup.object().shape({
+  text: yup.string().required('This field is required'),
+})
+
+export const AddTodoForm = ({ ...props }: AddTodoFormProps) => {
+  const { handleAdd } = useTodoDispatch()
+
+  const handleSubmit = ({ text }: typeof formInitialValues) => {
+    handleAdd(text)
+  }
+
+  return (
+    <Formik
+      initialValues={formInitialValues}
+      validationSchema={validationSchema}
+      onSubmit={handleSubmit}
+    >
+      <Form className={styles.className} {...props}>
+        <div className={styles.fieldWrapper}>
+          <Field as={Input} name="text" />
+          <Button type="submit">Add</Button>
+        </div>
+      </Form>
+    </Formik>
+  )
+}
