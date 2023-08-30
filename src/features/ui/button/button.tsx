c38f6f703bacda967e8510b48fa3ae7956ffd20d
Shaquille Galanto <shaquille.galanto18@gmail.com>
2023-08-30 23:25:33 +0800

    ✨ feat: implement disabled state of button ui component

--- src/features/ui/button/button.tsx
+++ src/features/ui/button/button.tsx
@@ -4,9 +4,10 @@ import styles from './styles.module.scss'
 import type { ButtonProps } from './types'
 
 export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ variant = 'primary', size = 'md', children, className, ...props }, ref) => (
+  ({ variant = 'primary', size = 'md', type = 'button', children, className, ...props }, ref) => (
     <button
       className={clsx(styles.button, styles[variant], styles[size], className)}
+      type={type}
       ref={ref}
       {...props}
     >
