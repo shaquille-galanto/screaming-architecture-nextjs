3df48c3db4724bbf3f75d4baf8700bb55b3f5951
Shaquille Galanto <shaquille.galanto18@gmail.com>
2023-10-03 23:10:28 +0800

    🔧 config: setup circular-dependency-plugin in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
+const CircularDependencyPlugin = require('circular-dependency-plugin')
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.plugins.push(
+        new CircularDependencyPlugin({
+          exclude: /a\.js|node_modules/,
+          failOnError: true,
+        }),
+      )
+    }
+
+    return config
+  },
+}
 
 module.exports = nextConfig
